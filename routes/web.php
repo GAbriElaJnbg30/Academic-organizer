08795d3f62e318b3dbaa1b75c7346cdc302b8b00
GAbriElaJnbg30 <21240200@leon.tecnm.mx>
2025-06-01 00:47:41 -0600

    Modificando index

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('iniciarsesion');
 })->name('iniciarsesion');
 
+
+
 Route::get('/acerca-de', function () {
     return view('acercade');
 })->name('acercade');
