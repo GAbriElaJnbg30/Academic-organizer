79d7e314dbae9bd4bcd288f25fea0d6800499633
GAbriElaJnbg30 <21240200@leon.tecnm.mx>
2025-06-01 10:54:54 -0600

    Agregar archivo startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+
+cd /home/site/wwwroot
+composer install --no-dev --optimize-autoloader
+
+php artisan config:clear
+php artisan cache:clear
+php artisan route:clear
+php artisan view:clear
+
 cd /home/site/wwwroot/public
-php -S 0.0.0.0:8080 -t .
+php -S 0.0.0.0:8080
