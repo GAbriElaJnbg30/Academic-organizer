8071a8a70f18ef08954aae9a94233a207d5626cb
GAbriElaJnbg30 <21240200@leon.tecnm.mx>
2025-06-01 12:06:08 -0600

    Subir sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd /home/site/wwwroot
+cd /home/site/wwwroot/public/index.php
 
 php artisan config:clear
 php artisan cache:clear
 php artisan route:clear
 php artisan view:clear
 
-cd /home/site/wwwroot/public
+cd /home/site/wwwroot/public/index.php
