eae6e5090fa10c9904022fcb905b139539250479
GAbriElaJnbg30 <21240200@leon.tecnm.mx>
2025-06-01 16:48:11 -0600

    Subir configuraciones

--- startup.sh
+++ startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd /home/site/wwwroot/public/index.php
+cd /home/site/wwwroot/public/
 
 php artisan config:clear
 php artisan cache:clear
 php artisan route:clear
 php artisan view:clear
 
-cd /home/site/wwwroot/public/index.php
+cd /home/site/wwwroot/public/
